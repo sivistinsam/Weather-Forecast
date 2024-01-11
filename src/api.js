d9f92829b4f93ff0a84e8022162f7b579df6a55f
Sandeep gupta <sivistinsam2@gmail.com>
2024-01-12 02:38:12 +0530

    First Commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+// const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "14240795c1mshfa31c9ee01c5af6p15fdd7jsn309f3de0ee2d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL =
+  "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6e75cc842f3a6fc28426d93d10d6861b";
+
+
